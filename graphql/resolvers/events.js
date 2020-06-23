8a17b1940e5d01d1543c4b2bc51d5bf134dfc5b0
Sagar <sampatel3636@gmail.com>
2020-06-23 18:05:07 -0500

    Adding authentication middleware

--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -1,6 +1,7 @@
-
+const { dateToString } = require("../../helpers/date");
 const { transformBooking, transformEvent } = require('./merge');
 const Event = require('../../models/event');
+const User = require('../../models/user');
 
 module.exports = {
     events: async () => {
@@ -13,19 +14,22 @@ module.exports = {
             throw err;
         }
     },
-    createEvent: async (args) => {
+    createEvent: async (args, req) => {
+        if (!req.isAuth) {
+            throw new Error('Unauthenticated!!!!');
+        }
         const event = new Event({
             title: args.eventInput.title,
             description: args.eventInput.description,
             price: +args.eventInput.price,
             date: dateToString(args.eventInput.date),
-            creator: "5ef22accc306a844bc0a0306"
+            creator: req.userId
         });
         let createdEvent;
         try {
             const result = await event.save()
             createdEvent = transformEvent(result);
-            const creator = await User.findById("5ef22accc306a844bc0a0306");
+            const creator = await User.findById(req.userId);
             if (!creator) {
                 throw new Error('User not found!!!.');
             }
