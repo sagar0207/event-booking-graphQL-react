debf281e287d9b86a162641607f259435f58eb83
Sagar <sampatel3636@gmail.com>
2020-06-23 10:32:52 -0500

    Adding Schema

---
+++ graphql/schema/index.js
@@ -0,0 +1,48 @@
+
+// using objext destructuring for storing properties from the object exported by graphql
+const { buildSchema } = require('graphql');
+
+module.exports = buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+            creator: User!
+        }
+
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+            createdEvents: [Event!]
+        }
+        
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input UserInput {
+            email: String!
+            password: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+
+        }
+        
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
+        }
+        
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `);
