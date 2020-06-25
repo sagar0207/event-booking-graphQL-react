08a8d0f14eb6410e61557ae305fa3652192b5d18
Sagar <sampatel3636@gmail.com>
2020-06-25 18:03:34 -0500

    Adding Model and Backdrop

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,8 +1,39 @@
 import React, { Component } from "react";
+import './Events.css';
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
 
 class EventsPage extends Component {
+
+  state = {
+    creating: false
+  };
+
+  startCreateEventHandler = () => {
+    this.setState({creating: true});
+  }
+
+  modelConfirmHandler = () => {
+    this.setState({creating: false});
+  };
+
+  modelCancelHandler = () => {
+    this.setState({ creating: false });
+  };
+
   render() {
-    return <h1>The Events Page</h1>;
+    return (
+      <React.Fragment>
+      {this.state.creating && <Backdrop />}
+      {this.state.creating && <Modal title = "Add Event" canCancel canConfirm onCancel={this.modelCancelHandler} onConfirm={this.modelConfirmHandler}>
+        <p>Modal Content</p>
+      </Modal>}
+      <div className="events-control">
+        <p>Share your Events!</p>
+        <button className="btn" onClick={this.startCreateEventHandler}>Create Event</button>
+      </div>
+      </React.Fragment>
+    );
   }
 }
 
