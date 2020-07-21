4cf975c0f080f6823049033df812f0f50ff58518
Sagar <sampatel3636@gmail.com>
2020-07-21 17:14:52 -0500

    Adding cancel booking funtionality

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import AuthContext from '../context/auth-context';
 import Spinner from '../components/Spinner/Spinner';
+import BookingList from '../components/Bookings/BookingList/BookingList';
 
 class BookingsPage extends Component {
   state = {
@@ -54,21 +55,54 @@ class BookingsPage extends Component {
     });
   };
 
+  deleteBookingHandler = bookingId => {
+    const requestBody = {
+      query: `
+        mutation {
+          cancelBooking(bookingId: "${bookingId}") {
+            _id
+            title
+          }
+        }
+      `
+    };
+
+    // fetch will return promise and so the err will be of networking issues rather than backend errors.
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.context.token
+      }
+    }).then(res => {
+      if (res.status !== 200 && res.status !== 201) {
+        throw new Error('Failed!');
+      }
+      return res.json();
+    }).then(resData => {
+      this.setState(prevState => {
+        const updatedBookings = prevState.bookings.filter(booking => {
+          return booking._id !== bookingId;
+        });
+        return {bookings: updatedBookings, isLoading: false};
+      });
+    }).catch(err => {
+      console.log(err);
+      if (this.isActive) {
+        this.setState({ isLoading: false });
+      }
+    });    
+  };
+
   render() {
     return (
       <React.Fragment>
         {this.state.isLoading ? (
           <Spinner />
         ) : (
-            <ul>
-              {this.state.bookings.map(booking => (
-                <li key={booking._id}>
-                  {booking.event.title} -{' '}
-                  {new Date(booking.createdAt).toLocaleDateString()}
-                </li>
-              ))}
-            </ul>
-          )}
+          <BookingList bookings={this.state.bookings} onDelete = {this.deleteBookingHandler}/>    
+        )}
       </React.Fragment>
     );
   }
